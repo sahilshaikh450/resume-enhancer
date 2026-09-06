eb1ed885900caa3a6e684214ae848d0a7fce80cc
Sahil Shaikh <sahilshaikh34088@gmail.com>
2026-09-06 11:53:47 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,6 +13,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.set('trust proxy', 1);
 app.use(cors({ origin: '*', credentials: false }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
